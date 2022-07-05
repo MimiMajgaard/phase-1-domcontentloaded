68ae07dfaa27831dfd4b4c91abbd28a9f1af9d6c
Mimi Majgaard <mimimajgaard@gmail.com>
2022-07-05 12:24:08 -0700

    commit tree 6f8f7e73c0a94f816b6e5f205429281b1d507999

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', cb)
+    document.querySelector('text')
+
+    
+
+function cb (event){
+    event.preventDefault()
+    document.querySelector('p').textContent = "This is really cool!"
+}
+
+
+
+
+
+// set up an event DOMContentLoaded listener 
+// use events' callback to target the paragraph element with id="text"
+// replace the text with "This is really cool"
+// use 'textContent' or 'innerHTML'
